b9420b0ece1102343b922ea218026afd8dca93f5
Victor <victorokolie001@gmail.com>
2025-06-26 22:45:16 +0100

    set uo shortener ui

--- shortener/static/shortener/js/main.js
+++ shortener/static/shortener/js/main.js
@@ -6,6 +6,7 @@ const myUrlsModal = new bootstrap.Modal(document.getElementById("myUrlsModal"));
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
+
 function enableFeaturHover() {
   if (window.innerWidth >= 992) {
     featuresLink.addEventListener("mouseenter", showFeatureDropdown);
@@ -37,12 +38,51 @@ muUrlsToggle.addEventListener("click", () => {
   document.querySelectorAll('.copy-btn').forEach(btn => {
     btn.addEventListener('click', function () {
       const link = this.getAttribute('data-link');
+      let initialText = this.innerHTML
       navigator.clipboard.writeText(link).then(() => {
         this.innerHTML = '<i class="bi bi-check-circle-fill"></i>';
+        showAlert('success', 'Link copied to clipboard!');
         setTimeout(() => {
-          this.innerHTML = '<i class="bi bi-clipboard"></i>';
+          this.innerHTML = initialText;
         }, 2000);
       });
     });
   });
 
+document.addEventListener("DOMContentLoaded", function () {
+    const consentBanner = document.getElementById("cookieConsent");
+    const acceptBtn = document.getElementById("acceptCookies");
+
+    if (!localStorage.getItem("shortenerCookieAccepted")) {
+      consentBanner.classList.remove("d-none");
+    }
+
+    acceptBtn.addEventListener("click", function () {
+      localStorage.setItem("shortenerCookieAccepted", "true");
+      consentBanner.classList.add("d-none");
+    });
+  });
+
+function showAlert(type = 'success', message = 'Action completed') {
+    const toastEl = document.getElementById('globalToast');
+    const toastBody = document.getElementById('globalToastBody');
+
+    const toast = bootstrap.Toast.getOrCreateInstance(toastEl);
+
+    // background color class based on type
+    const bgClassMap = {
+      success: 'bg-success',
+      warning: 'bg-dark',
+      error: 'bg-danger',
+      danger: 'bg-danger', // alias
+      info: 'bg-info',
+    };
+
+    // Reset previous classes and content
+    toastEl.className = 'toast align-items-center text-white border-0';
+    toastEl.classList.add(bgClassMap[type] || 'bg-success');
+
+    toastBody.innerText = message;
+
+    toast.show();
+  }
--- shortener/static/shortener/js/shortener.js
+++ shortener/static/shortener/js/shortener.js
@@ -2,6 +2,9 @@ const tryItNow = document.getElementById('tryItNow')
 const urlInput = document.getElementById('urlInput')
 const shortenBtn = document.getElementById('shortenBtn')
 const urlShortener = document.getElementById('urlShortener')
+const resultCard = document.getElementById("shortenedResult");
+const container = document.getElementById("ShortenerContainer");
+const shortenAnotherBtn = document.getElementById("shortenAnother");
 
 
 tryItNow.addEventListener('click', ()=> {
@@ -12,5 +15,28 @@ tryItNow.addEventListener('click', ()=> {
 urlShortener.addEventListener('submit', (e)=> {
     e.preventDefault()
     console.log('url submitted')
+
+    if (urlInput.value == "" || urlInput.value == null) {
+        showAlert('warning', 'Please enter a valid URL.');
+        return
+    }
+
+    urlShortener.classList.add("fade-out");
+    setTimeout(() => {
+      urlShortener.classList.add("d-none");
+      resultCard.classList.remove("d-none");
+      resultCard.classList.remove("fade-out");
+      resultCard.classList.add("fade-in");
+    }, 400);
 })
-shortenBtn.addEventListener
+
+shortenAnotherBtn.addEventListener("click", () => {
+    resultCard.classList.add("fade-out");
+    setTimeout(() => {
+      resultCard.classList.add("d-none");
+      urlShortener.classList.remove("d-none");
+      urlShortener.classList.remove("fade-out");
+      urlShortener.reset()
+      urlShortener.classList.add("fade-in");
+    }, 400);
+  });
